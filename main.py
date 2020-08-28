7809a4c5a44d8b7d5cf07e287e160793b794861d
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-28 22:18:16 +0900

    4.1 Introduction to Flask

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs
 from stackoverflow import extract_stackoverflow_jobs
 from save import save_to_csv
+from flask import Flask
 
-last_indeed_page = extract_indeed_pages()
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
+app = Flask("job scrapper")
 
-stackoverflow_jobs = extract_stackoverflow_jobs()
 
-jobs = indeed_jobs + stackoverflow_jobs
-save_to_csv(jobs)
+@app.route('/')
+def index():
+  return 'This is Home!'
+
+
+@app.route('/contact')
+def contact():
+  return "Please contact me on email"
+
+
+app.run(host="0.0.0.0")
+
+# last_indeed_page = extract_indeed_pages()
+# indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+# stackoverflow_jobs = extract_stackoverflow_jobs()
+
+# jobs = indeed_jobs + stackoverflow_jobs
+# save_to_csv(jobs)
