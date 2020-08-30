9d8e4cf1b0b27157a15e5015fc9d9410b7a090bc
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-30 21:50:59 +0900

    당근마켓 적용 완료

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from danggn_item import get_danggn_items
 from naver_word import get_naver_word
 from save import save_to_csv
+from flask import Flask, render_template, redirect, request
 
 
-def run():
-    items = get_danggn_items("서울특별시")
+app = Flask("my page")
+
+
+@app.route('/')
+def index():
     words = get_naver_word()
+    return render_template("index.html",
+                           words=words
+                           )
 
-    for word in words:
-        print(word.get("word_area"), word.get("mean"))
 
-    for item in items:
-        print(item.get("img"), item.get("title"), item.get("price"))
+@app.route('/search')
+def search():
+    region = request.args.get("region")
+    items = get_danggn_items(region)
+    return render_template("danggn.html",
+                           items=items)
 
-    save_to_csv(items)
+
+app.run(host="0.0.0.0")
