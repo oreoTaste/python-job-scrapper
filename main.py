de54fada054a50eb9bb5a9eff60d32df65564bb6
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-31 14:19:36 +0900

    fake db 작성

--- main.py
+++ main.py
@@ -5,26 +5,44 @@ from flask import Flask, render_template, redirect, request
 
 
 app = Flask("my page")
+db0 = {}
+db1 = {}
+db2 = {}
 
 
 @app.route('/')
 @app.route('/<path:keyword>')
 def index(keyword=''):
-    words = get_naver_word()
-    districts = district1()
-    if keyword == '':
-        return render_template("index.html",
-                               words=words,
-                               districts=districts
-                               )
+    print("신호가 왔습니다")
+    global db0
+    if db0 != {}:
+        words = db0
     else:
-        districts2 = district2(keyword)
-        return render_template("index.html",
-                               words=words,
-                               keyword=keyword,
-                               districts=districts,
-                               districts2=districts2
-                               )
+        words = get_naver_word()
+        db0 = words
+
+    global db1
+    if db1 != {}:
+        districts = db1
+    else:
+        districts = district1()
+        db1 = districts
+
+    if keyword == '':
+        districts2 = ''
+    else:
+        global db2
+        try:
+            districts2 = db2[keyword]
+        except:
+            districts2 = district2(keyword)
+            db2[keyword] = districts2
+    return render_template("index.html",
+                           words=words,
+                           keyword=keyword,
+                           districts=districts,
+                           districts2=districts2
+                           )
 
 
 @app.route('/search')
